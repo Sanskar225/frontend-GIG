7a6f5a809404035b47a9f83b9bd522416b3d4158
sanskar sinha <sanskarsinha225@gmail.com>
2026-01-14 22:13:52 +0530

    corrections in api

--- src/api/bid.api.js
+++ src/api/bid.api.js
@@ -2,12 +2,12 @@ import axios from './axios';
 
 export const bidAPI = {
   submitBid: async (bidData) => {
-    const response = await axios.post('/bids', bidData);
+    const response = await axios.post('/api/bids', bidData);
     return response.data;
   },
 
   getBidsForGig: async (gigId) => {
-    const response = await axios.get(`/bids/${gigId}`);
+    const response = await axios.get(`/api/bids/${gigId}`);
     return response.data;
   },
 
@@ -17,17 +17,17 @@ export const bidAPI = {
   },
 
   hireBid: async (bidId) => {
-    const response = await axios.patch(`/bids/${bidId}/hire`);
+    const response = await axios.patch(`/api/bids/${bidId}/hire`);
     return response.data;
   },
 
   updateBid: async (bidId, bidData) => {
-    const response = await axios.patch(`/bids/${bidId}`, bidData);
+    const response = await axios.patch(`/api/bids/${bidId}`, bidData);
     return response.data;
   },
 
   deleteBid: async (bidId) => {
-    const response = await axios.delete(`/bids/${bidId}`);
+    const response = await axios.delete(`/api/bids/${bidId}`);
     return response.data;
   },
 };
